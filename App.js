01e6d2afd89f2d5ac1179c75568dad6a9688f1d3
Akul Varshney <allenv213@gmail.com>
2024-02-07 16:12:03 +0530

    .

--- App.js
+++ App.js
@@ -1,29 +1,51 @@
-import React, { useState } from "react";
-import { View, ActivityIndicator, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, ActivityIndicator, StyleSheet, Image } from "react-native";
 import { WebView } from "react-native-webview";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
+  const [showLogo, setShowLogo] = useState(true);
 
-  const handleLoadStart = () => {
-    setLoading(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowLogo(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const handleLoad = (syntheticEvent) => {
+    const { nativeEvent } = syntheticEvent;
+
+    if (nativeEvent.loading) {
+      // Page start loading
+      setLoading(true);
+    } else {
+      // Page finished loading
+      setLoading(false);
+    }
   };
-
-  const handleLoadEnd = () => {
-    setLoading(false);
-  };
-
   return (
     <View style={{ flex: 1 }}>
       <WebView
-        source={{ uri: "https://crm.bharatcertis.com/auth/login/" }}
+        source={{ uri: "https://crm.bharatcertis.com/admin/dashboard" }}
         style={{ flex: 1 }}
-        onLoadStart={handleLoadStart}
-        onLoadEnd={handleLoadEnd}
+        onLoad={handleLoad}
       />
-      {loading && (
+
+      {showLogo && (
+        <View style={styles.loader2}>
+          <Image style={styles.image} source={require("./assets/icon.png")} />
+        </View>
+      )}
+
+      {loading && !showLogo && (
         <View style={styles.loader}>
-          <ActivityIndicator size="large" color="#0000ff" />
+          <ActivityIndicator
+            style={{ marginTop: 20 }}
+            size="large"
+            color="#0000ff"
+          />
         </View>
       )}
     </View>
@@ -31,11 +53,29 @@ const App = () => {
 };
 
 const styles = {
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   loader: {
     ...StyleSheet.absoluteFillObject,
     backgroundColor: "rgba(255,255,255,0.8)",
     justifyContent: "center",
     alignItems: "center",
+  },
+  loader2: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderWidth: 1,
+    borderColor: "white",
+    aspectRatio: 1,
   },
 };
 
